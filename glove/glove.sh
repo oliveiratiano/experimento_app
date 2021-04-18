89d472815f8a709e218a557b8d111e2920345716
oliveiratiano <oliveiratiano@gmail.com>
2021-04-18 00:21:15 -0300

    Update glove.sh

--- glove/glove.sh
+++ glove/glove.sh
@@ -21,6 +21,9 @@ X_MAX=10
 
 chmod +x $BUILDDIR/vocab_count
 $BUILDDIR/vocab_count -min-count $VOCAB_MIN_COUNT -verbose $VERBOSE < $CORPUS > $VOCAB_FILE
+chmod +x $BUILDDIR/cooccur
 $BUILDDIR/cooccur -memory $MEMORY -vocab-file $VOCAB_FILE -verbose $VERBOSE -window-size $WINDOW_SIZE < $CORPUS > $COOCCURRENCE_FILE
+chmod +x $BUILDDIR/shuffle
 $BUILDDIR/shuffle -memory $MEMORY -verbose $VERBOSE < $COOCCURRENCE_FILE > $COOCCURRENCE_SHUF_FILE
+chmod +x $BUILDDIR/glove
 $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
