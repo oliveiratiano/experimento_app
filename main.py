b7711b7f76c9e2c06938b99c221f49c279875517
oliveiratiano <oliveiratiano@gmail.com>
2021-04-19 20:26:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from src import transformer
 import warnings
 import os, shutil
+import numpy as np
 warnings.filterwarnings("ignore")
 
 transformer.extrair_corpus()
-documentos_validos = transformer.ler_documentos_validos(quantidade=1000)
+documentos_validos = transformer.ler_documentos_validos()
 
 os.mkdir('resultados')
 
-lista_k = [2,3,4,5]
-grid_minfreqs = [0]
-grid_stopwords = [True]
-grid_ica = [True]
-grid_tesauro = [True]
+lista_k = np.arange(2,5)
+grid_minfreqs = [0, 50, 100]
+grid_stopwords = [True, False]
+grid_ica = [True, False]
+grid_tesauro = [True, False]
 grid_dimensoes = [100]
 transformer.rodar_experimento(documentos_validos, grid_minfreqs, grid_stopwords, grid_ica, grid_tesauro, grid_dimensoes, lista_k)
 os.remove('tesauro_stf.csv')
