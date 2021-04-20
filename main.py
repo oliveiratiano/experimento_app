8ac8f66340dc9a893e3f94f7dff43223474de665
oliveiratiano <oliveiratiano@gmail.com>
2021-04-19 21:07:04 -0300

    executar grid

--- main.py
+++ main.py
@@ -11,14 +11,15 @@ documentos_validos = transformer.ler_documentos_validos(quantidade = 1000)
 os.mkdir('resultados')
 
 lista_k = np.arange(2,5)
-grid_minfreqs = [0]
-grid_stopwords = [True]
-grid_ica = [True]
-grid_tesauro = [True]
+grid_minfreqs = [0, 50, 100]
+grid_stopwords = [True, False]
+grid_ica = [True, False]
+grid_tesauro = [True, False]
 grid_dimensoes = [100]
 try:
     transformer.rodar_experimento(documentos_validos, grid_minfreqs, grid_stopwords, grid_ica, grid_tesauro, grid_dimensoes, lista_k)
-except Exception:
+except Exception e:
+    import pdb; pdb.set_trace()
     traceback.print_exc()
 
 os.remove('tesauro_stf.csv')
